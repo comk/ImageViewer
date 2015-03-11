c858eb948558104cc619f5c1ca6718be3ca32eca
dhandzm@gmail.com <dhandzm@gmail.com>
2015-03-11 16:45:11 +0800

    Initial commit

---
+++ app/src/main/java/com/yiguo/daihai/imageviewer/ImageViewer.java
@@ -0,0 +1,143 @@
+package com.yiguo.daihai.imageviewer;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+/**
+ * Created by daihai on 2015/3/11.
+ */
+public class ImageViewer extends LinearLayout{
+    private static ImageViewer instance;
+
+    private ImageViewConfig config;
+
+    public ImageViewer(Context context) {
+        super(context);
+    }
+
+    public ImageViewer(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public static ImageViewer getInstance(Context context){
+        if(instance == null) {
+            if(context != null)
+                instance = new ImageViewer(context);
+            else
+                throw new NullPointerException("Context can not be null");
+        }
+        return instance;
+    }
+
+    public void initImageViewer(ImageViewConfig config){
+        this.config = config;
+
+
+        if(LinearLayout.HORIZONTAL == config.direction){ //水平方向
+            if(ThumbnailsPosition.POSITION_LEFT == config.thumbnailsPosition){
+
+            }else if(ThumbnailsPosition.POSITION_RIGHT == config.thumbnailsPosition){
+
+            }
+        }else if(LinearLayout.VERTICAL == config.direction){//垂直方向
+            if(ThumbnailsPosition.POSITION_TOP == config.thumbnailsPosition){
+
+            }else if(ThumbnailsPosition.POSITION_BOTTOM == config.thumbnailsPosition){
+
+            }
+        }
+
+
+    }
+
+    public static class ThumbnailsPosition{
+        /** 设置小图在上方 **/
+        public static final int POSITION_TOP = 0;
+        /** 设置小图在下方 **/
+        public static final int POSITION_BOTTOM = 1;
+        /** 设置小图在左方 **/
+        public static final int POSITION_LEFT = 2;
+        /** 设置小图在右方 **/
+        public static final int POSITION_RIGHT = 3;
+
+    }
+
+
+
+    public class ImageViewConfig{
+        /** 图片是否可伸缩 **/
+        private boolean isScaleable = true;
+
+        /** 图片是否可删除 **/
+        private boolean isDeleteable = true;
+
+        /** 是否显示缩略图 **/
+        private boolean isShowThumbnail = true;
+
+        /** 显示缩略图的位置 **/
+        private int thumbnailsPosition = ThumbnailsPosition.POSITION_BOTTOM;
+
+        private float thumbnailsPercentage;
+
+        /** 大图和小图的布局方式 一种是垂直 一种是水平  LinearLayout.HORIZONTAL   LinearLayout.VERTICAL **/
+        private int direction = LinearLayout.VERTICAL;
+
+        public boolean isScaleable() {
+            return isScaleable;
+        }
+
+        public float getThumbnailsPercentage() {
+            return thumbnailsPercentage;
+        }
+
+        public ImageViewConfig setThumbnailsPercentage(float thumbnailsPercentage) {
+            this.thumbnailsPercentage = thumbnailsPercentage;
+            return this;
+        }
+
+        public ImageViewConfig setScaleable(boolean isScaleable) {
+            this.isScaleable = isScaleable;
+            return this;
+        }
+
+        public boolean isDeleteable() {
+            return isDeleteable;
+        }
+
+        public ImageViewConfig setDeleteable(boolean isDeleteable) {
+            this.isDeleteable = isDeleteable;
+            return this;
+        }
+
+        public boolean isShowThumbnail() {
+            return isShowThumbnail;
+        }
+
+        public ImageViewConfig setShowThumbnail(boolean isShowThumbnail) {
+            this.isShowThumbnail = isShowThumbnail;
+            return this;
+        }
+
+        public int getThumbnailsPosition() {
+            return thumbnailsPosition;
+        }
+
+        public ImageViewConfig setThumbnailsPosition(int thumbnailsPosition) {
+            this.thumbnailsPosition = thumbnailsPosition;
+            return this;
+        }
+
+        public int getDirection() {
+            return direction;
+        }
+
+        public ImageViewConfig setDirection(int direction) {
+            this.direction = direction;
+            return this;
+        }
+    }
+
+
+}
